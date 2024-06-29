541282cca90b1aa65bc88cab63382f5a0fcaf05f
AKadmani <adam.kadmani1@hotmail.com>
2024-06-29 18:07:54 +0200

    Optimized foregroundobj.m
    Precompute params
    Preallocate L
    Cleaned

--- lib/foregroundobj.m
+++ lib/foregroundobj.m
@@ -1,51 +1,53 @@
-function [FGVertex,foreobj] = foregroundobj(img,vanishing_point,estimatedVertex,min_y_fore,max_y_fore,min_x_fore,max_x_fore)
+function [FGVertex, foreobj] = foregroundobj(img, vanishing_point, estimatedVertex, min_y_fore, max_y_fore, min_x_fore, max_x_fore)
 
-% original coordination of foregroundobj
-% min_y_fore=1300;max_y_fore=1350;
-% min_x_fore=1300;max_x_fore=1350;
-pic_num=ceil(max(size(img(:,:)))/5);
+% Precompute size parameters
+pic_num = ceil(max(size(img(:,:))) / 5);
+
+% Floor the inputs
 min_y_fore = floor(min_y_fore);
 min_x_fore = floor(min_x_fore);
 max_y_fore = floor(max_y_fore);
 max_x_fore = floor(max_x_fore);
 
-y_sp_fore=[max_y_fore;max_y_fore;min_y_fore;min_y_fore]';
-x_sp_fore=[min_x_fore;max_x_fore;max_x_fore;min_x_fore]';
+% 2D coordinates of foreground object
+x_sp_fore = [min_x_fore; max_x_fore; max_x_fore; min_x_fore]';
+y_sp_fore = [max_y_fore; max_y_fore; min_y_fore; min_y_fore]';
 
+% Vertex coordinates
 FGVertex = [min_x_fore max_x_fore max_x_fore min_x_fore;
-    max_y_fore max_y_fore min_y_fore min_y_fore]
+            max_y_fore max_y_fore min_y_fore min_y_fore];
 
-
-% 2D coordination
+% Initial 2D coordinates
 foreobj = [x_sp_fore; y_sp_fore];
 
-x = estimatedVertex(1,5)-(estimatedVertex(2,5)-y_sp_fore(3))*(estimatedVertex(1,5)-estimatedVertex(1,1)) / (estimatedVertex(2,5)-estimatedVertex(2,1));
+% Calculate x coordinate using estimatedVertex
+x = estimatedVertex(1, 5) - (estimatedVertex(2, 5) - y_sp_fore(3)) * (estimatedVertex(1, 5) - estimatedVertex(1, 1)) / (estimatedVertex(2, 5) - estimatedVertex(2, 1));
+fore = [x; y_sp_fore(1)];
 
-fore = [x;y_sp_fore(1)];
+% Add z coordinate (3D coordinates)
+foreobj = [foreobj; zeros(1, 4)];
 
-% 3D coordination
-foreobj = [foreobj; zeros(1,4)];
-L=zeros(12,1);
+% Preallocate distances array
+L = zeros(12, 1);
 
-% Distance of vanish point and estimated vertex
-for i=[1:12]
-   L(i)=pdist([[vanishing_point(1),vanishing_point(2)]',estimatedVertex(:,i)]','euclidean');
+% Ensure vanishing_point and estimatedVertex are both 2xN matrices
+vanishing_point = vanishing_point(:)'; % Ensure vanishing_point is a row vector
+
+% Distance between vanishing point and estimated vertex
+for i = 1:12
+   L(i) = pdist([vanishing_point; estimatedVertex(:, i)'], 'euclidean');
 end
 
-% Find the z of the foregroundobj through the proportional relationship
-K = pdist([[vanishing_point(1),vanishing_point(2)]',fore]','euclidean');
+% Distance of vanishing point and foreground point
+K = pdist([vanishing_point; fore'], 'euclidean');
 
-y_ref = max(estimatedVertex(2,5),estimatedVertex(2,3));
-y_cut = (y_sp_fore(1)+y_sp_fore(3))/2;
-foreobj(3,:) =abs(y_cut-vanishing_point(1))/abs(y_ref-vanishing_point(1));
+% Find the z coordinate of the foreground object
+y_ref = max(estimatedVertex(2, 5), estimatedVertex(2, 3));
+y_cut = (y_sp_fore(1) + y_sp_fore(3)) / 2;
+foreobj(3, :) = abs(y_cut - vanishing_point(2)) / abs(y_ref - vanishing_point(2));
 
-% foreobj(3,:) =max(L(5),L(3))/K;
-foreobj([1,2],:)=(foreobj([1,2],:)-[vanishing_point(1),vanishing_point(2)]').*foreobj(3,:);
-foreobj(3,:) = foreobj(3,:)*pic_num;
+% Apply proportional relationship
+foreobj([1, 2], :) = (foreobj([1, 2], :) - vanishing_point') .* foreobj(3, :);
+foreobj(3, :) = foreobj(3, :) * pic_num;
 
 end
-
-
-
-
-

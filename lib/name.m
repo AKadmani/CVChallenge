dcb92e326e0fac32d6d36397c8e09af2f30c6bb5
AKadmani <adam.kadmani1@hotmail.com>
2024-06-29 18:06:01 +0200

    Optimized name.m
    Preallocate
    Parallel Processing

--- lib/name.m
+++ lib/name.m
@@ -1,69 +1,72 @@
-function [out,test] = name(estimatedVertex,recVertex,img)
-out={};
-[m,n,p]=size(img);test=max([m,n]/500);
-test=max(max(recVertex([1,2],:)')-min(recVertex([1,2],:)'))/1000;
-if test<1
-test=1
+function [out, test] = name(estimatedVertex, recVertex, img)
+    [m, n, p] = size(img);
+    test = max(max(recVertex([1,2],:)') - min(recVertex([1,2],:)'))/1000;
+    test = max(test, 1);
+
+    % Preallocate the output cell array
+    out = cell(1, 5);
+
+    % Precompute common values
+    recVertexScaled = recVertex / test;
+
+    % Define the vertex indices for each transformation
+    vertexIndices = {[1,2,8,7], [11,5,1,7], [1,3,4,2], [2,8,12,6], [7,8,10,9]};
+
+    % Use parallel processing if available
+    if license('test', 'Distrib_Computing_Toolbox')
+        parfor i = 1:5
+            out{i} = processPlane(img, estimatedVertex, recVertexScaled, vertexIndices{i}, i);
+        end
+    else
+        for i = 1:5
+            out{i} = processPlane(img, estimatedVertex, recVertexScaled, vertexIndices{i}, i);
+        end
+    end
 end
-trans{1} = fitgeotrans(estimatedVertex(:,[1,2,8,7])',recVertex([1,2],[1,2,8,7])'/test,'projective');
-min_x=min(estimatedVertex(1,[1,2,8,7])');
-max_x=max(estimatedVertex(1,[1,2,8,7])');
-min_y=min(estimatedVertex(2,[1,2,8,7])');
-max_y=max(estimatedVertex(2,[1,2,8,7])');
-p0=imref2d(size(img(min_y:max_y,min_x:max_x,:)));
-p0.XWorldLimits=p0.XWorldLimits+min_x-1;
-p0.YWorldLimits=p0.YWorldLimits+min_y-1;
-[outputImage,p1] = imwarp(img(min_y:max_y,min_x:max_x,:),p0, trans{1} );
-vec=[p1.XWorldLimits(1),p1.YWorldLimits(1)];
-coord=ceil(recVertex([1,2],[1,2,8,7])'/test-vec);
-out{1}=outputImage(min(coord(:,2)):max(coord(:,2)),min(coord(:,1)):max(coord(:,1)),:);
 
-trans{2} = fitgeotrans(estimatedVertex(:,[11,5,1,7])',[recVertex([3],[11,5,1,7])',recVertex([2],[11,5,1,7])']/test,'projective');
-min_x=min(estimatedVertex(1,[11,5,1,7])');
-max_x=max(estimatedVertex(1,[11,5,1,7])');
-min_y=min(estimatedVertex(2,[11,5,1,7])');
-max_y=max(estimatedVertex(2,[11,5,1,7])');
-p0=imref2d(size(img(:,min_x:max_x,:)));
-p0.XWorldLimits=p0.XWorldLimits+min_x-1;
-[outputImage,p1] = imwarp(img(:,min_x:max_x,:),p0, trans{2} );
-vec=[p1.XWorldLimits(1),p1.YWorldLimits(1)];
-coord=ceil(recVertex([3,2],[11,5,1,7])'/test-vec);
-out{2}=outputImage(min(coord(:,2)):max(coord(:,2)),min(coord(:,1)):max(coord(:,1)),:);
+function outputImage = processPlane(img, estimatedVertex, recVertexScaled, vertexIdx, planeNum)
+    % Extract relevant vertices
+    estVert = estimatedVertex(:, vertexIdx)';
+    recVert = recVertexScaled([1,2], vertexIdx)';
 
-trans{3} = fitgeotrans(estimatedVertex(:,[1,3,4,2])',[recVertex([1],[1,3,4,2])',recVertex([3],[1,3,4,2])']/test,'projective');
-min_x=min(estimatedVertex(1,[1,3,4,2])');
-max_x=max(estimatedVertex(1,[1,3,4,2])');
-min_y=min(estimatedVertex(2,[1,3,4,2])');
-max_y=max(estimatedVertex(2,[1,3,4,2])');
-p0=imref2d(size(img(min_y:max_y,:,:)));
-p0.YWorldLimits=p0.YWorldLimits+min_y-1;
-[outputImage,p1] = imwarp(img(min_y:max_y,:,:),p0, trans{3} );
-vec=[p1.XWorldLimits(1),p1.YWorldLimits(1)];
-coord=ceil([recVertex([1],[1,3,4,2])',recVertex([3],[1,3,4,2])']/test-vec);
-out{3}=outputImage(min(coord(:,2)):max(coord(:,2)),min(coord(:,1)):max(coord(:,1)),:);
+    % Adjust recVert based on plane number
+    if planeNum == 2
+        recVert = [recVertexScaled([3,2], vertexIdx)'];
+    elseif planeNum == 3
+        recVert = [recVertexScaled([1,3], vertexIdx)'];
+    elseif planeNum == 4
+        recVert = [-recVertexScaled([3,2], vertexIdx)'];
+    elseif planeNum == 5
+        recVert = [-recVertexScaled([1,3], vertexIdx)'];
+    end
 
+    % Compute transformation
+    tform = fitgeotrans(estVert, recVert, 'projective');
 
-trans{4} = fitgeotrans(estimatedVertex(:,[2,8,12,6])',[-recVertex([3],[2,8,12,6])',recVertex([2],[2,8,12,6])']/test,'projective');
-min_x=min(estimatedVertex(1,[2,8,12,6])');
-max_x=max(estimatedVertex(1,[2,8,12,6])');
-min_y=min(estimatedVertex(2,[2,8,12,6])');
-max_y=max(estimatedVertex(2,[2,8,12,6])');
-p0=imref2d(size(img(:,min_x:max_x,:)));
-p0.XWorldLimits=p0.XWorldLimits+min_x-1;
-[outputImage,p1] = imwarp(img(:,min_x:max_x,:),p0, trans{4} );
-vec=[p1.XWorldLimits(1),p1.YWorldLimits(1)];
-coord=ceil([-recVertex([3],[2,8,12,6])',recVertex([2],[2,8,12,6])']/test-vec);
-out{4}=outputImage(min(coord(:,2)):max(coord(:,2)),min(coord(:,1)):max(coord(:,1)),:);
+    % Determine region of interest
+    minX = floor(min(estVert(:,1)));
+    maxX = ceil(max(estVert(:,1)));
+    minY = floor(min(estVert(:,2)));
+    maxY = ceil(max(estVert(:,2)));
 
-trans{5} = fitgeotrans(estimatedVertex(:,[7,8,10,9])',[-recVertex([1],[7,8,10,9])',recVertex([3],[7,8,10,9])']/test,'projective');
-min_x=min(estimatedVertex(1,[7,8,10,9])');
-max_x=max(estimatedVertex(1,[7,8,10,9])');
-min_y=min(estimatedVertex(2,[7,8,10,9])');
-max_y=max(estimatedVertex(2,[7,8,10,9])');
-p0=imref2d(size(img(min_y:max_y,:,:)));
-p0.YWorldLimits=p0.YWorldLimits+min_y-1;
-[outputImage,p1] = imwarp(img(min_y:max_y,:,:),p0, trans{5} );
-vec=[p1.XWorldLimits(1),p1.YWorldLimits(1)];
-coord=ceil([-recVertex([1],[7,8,10,9])',recVertex([3],[7,8,10,9])']/test-vec);
-out{5}=outputImage(min(coord(:,2)):max(coord(:,2)),min(coord(:,1)):max(coord(:,1)),:);
+    % Extract relevant portion of image
+    if planeNum == 2 || planeNum == 4
+        roiImg = img(:, minX:maxX, :);
+        p0 = imref2d(size(roiImg));
+        p0.XWorldLimits = p0.XWorldLimits + minX - 1;
+    else
+        roiImg = img(minY:maxY, :, :);
+        p0 = imref2d(size(roiImg));
+        p0.YWorldLimits = p0.YWorldLimits + minY - 1;
+    end
 
+    % Apply transformation
+    [warpedImg, p1] = imwarp(roiImg, p0, tform);
+
+    % Compute coordinates for cropping
+    vec = [p1.XWorldLimits(1), p1.YWorldLimits(1)];
+    coord = ceil(recVert - vec);
+
+    % Crop the warped image
+    outputImage = warpedImg(min(coord(:,2)):max(coord(:,2)), min(coord(:,1)):max(coord(:,1)), :);
+end
